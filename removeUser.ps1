61ee93b3203ef53137aa43ff86555b82a53cb9e1
Steven Burrows <60275700+stevenjburrows@users.noreply.github.com>
2021-11-10 17:05:07 +0000

    used to remove users after they have left a compnay

---
+++ removeUser.ps1
@@ -0,0 +1,30 @@
+﻿# to run use .\removeUser.ps1 -upn <their upn or email address>
+# if no upn is supplied it will ask for one
+# by Steven Burrows
+# User for when people leave a company
+# this will convert their mailbox to a shared mailbox
+# remove all their licenses and block their sign in
+
+Connect-ExchangeOnline
+
+#if no upn is supllied it asks for one
+if ($upn -eq $null) {
+$upn = read-host -Prompt "Please enter a upn or email address" 
+}
+
+# Sets mailbos to shared
+Set-Mailbox -Identity $upn -Type Shared
+
+#removes all licenses
+(get-MsolUser -UserPrincipalName $upn).licenses.AccountSkuId |
+foreach{
+    Set-MsolUserLicense -UserPrincipalName $upn -RemoveLicenses $_
+}
+
+# block user sign in 
+Set-MsolUser -UserPrincipalName $upn  -BlockCredential $true
+
+# reminder
+Write-host "*******************************"
+Write-host "Remember to update SkyKick"
+Write-host "*******************************"
