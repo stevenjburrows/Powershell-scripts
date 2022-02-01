4cab3b21ccbd8a3836f1f549679bf73a5b13a6e0
Steven Burrows <60275700+stevenjburrows@users.noreply.github.com>
2022-02-01 16:07:40 +0000

    Enable and update the auto reply for all shared mailboxes  with a particular domain

---
+++ ShareMailboxAutoReply.ps1
@@ -0,0 +1,22 @@
+﻿# set a default auto reply to all shared mailboxes with a particular domain
+# use the param domain to incluse *@domain.com
+# Developed by Steven Burrows
+
+params($domain, $message)
+
+#if no domain is supllied it asks for one
+if ($domain -eq $null) {
+    $domain = read-host -Prompt "Please enter a domain name and incluse a *@ before it e.g. *@domain.com" 
+}
+
+if ($message -eq $null) {
+    $message = read-host -Prompt "Please enter a reply message that you would like included" 
+}
+
+Connect-ExchangeOnline
+$mailboxes = Get-Mailbox -RecipientTypeDetails SharedMailbox -ResultSize unlimited | Where-Object {$_.emailAddresses -like $domain } | Select-Object PrimarySmtpAddress
+foreach($mailbox in $mailboxes){ 
+    Set-MailboxAutoReplyConfiguration -Identity $mailbox.PrimarySmtpAddress -AutoReplyState Enabled -InternalMessage "Enter your message here"
+
+}
+
