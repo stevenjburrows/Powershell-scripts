bf82b051b6e56f573ce3eb0363fb9f2cd8fbae9f
Steven Burrows <60275700+stevenjburrows@users.noreply.github.com>
2022-03-03 16:58:39 +0000

    updated the file for more functionality

--- removeUser.ps1
+++ removeUser.ps1
@@ -1,22 +1,29 @@
 ﻿# to run use .\removeUser.ps1 -upn <their upn or email address>
 # if no upn is supplied it will ask for one
+# use -login for login propmts without it it assumes your shell is already logged in
+# -message allows you to add an auto reply
 # by Steven Burrows
 # User for when people leave a company
 # this will convert their mailbox to a shared mailbox
 # remove all their licenses and block their sign in
 
 #params
-param($upn)
+param($upn, [switch]$login, $message)
 
-Connect-ExchangeOnline
 
 #if no upn is supllied it asks for one
 if ($upn -eq $null) {
 $upn = read-host -Prompt "Please enter a upn or email address" 
 }
 
+if($login -ne $null) {
+Connect-MsolService
+Connect-ExchangeOnline
+}
+
 # Sets mailbos to shared
 Set-Mailbox -Identity $upn -Type Shared
+Write-host "converted to a shared mailbox" -BackgroundColor Red -ForegroundColor White
 
 #removes all licenses
 (get-MsolUser -UserPrincipalName $upn).licenses.AccountSkuId |
@@ -24,8 +31,17 @@ foreach{
     Set-MsolUserLicense -UserPrincipalName $upn -RemoveLicenses $_
 }
 
+Write-host "licenses removed" -BackgroundColor Red -ForegroundColor White
 # block user sign in 
 Set-MsolUser -UserPrincipalName $upn  -BlockCredential $true
+
+Write-host "sign in blocked" -BackgroundColor Red -ForegroundColor White
+
+if($message -ne $null){
+Set-MailboxAutoReplyConfiguration -Identity $upn -AutoReplyState Enabled -InternalMessage $message
+Write-host "Auto reply added" -BackgroundColor Red -ForegroundColor White
+}
+
 
 # reminder
 Write-host "*******************************"
